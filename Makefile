f216cea8c317fc1c39d92d21119c73e2fb3541b4
Bastian Koppelmann <kbastian@mail.uni-paderborn.de>
2022-05-23 17:24:56 +0200

    Pyke: Add first version of tb
    Signed-off-by: Bastian Koppelmann <kbastian@mail.uni-paderborn.de>

--- Makefile
+++ Makefile
@@ -1,15 +1,41 @@
-base_dir   = $(abspath .)
-src_dir    = $(base_dir)/src/main
-gen_dir    = $(base_dir)/generated-src
-out_dir    = $(base_dir)/outputs
+BASE_DIR   = $(abspath .)
+SRC_DIR    = $(BASE_DIR)/src/main
+GEN_DIR    = $(BASE_DIR)/generated-src
+OUT_DIR    = $(BASE_DIR)/outputs
 
 SBT       = sbt
-SBT_FLAGS = -ivy $(base_dir)/.ivy2
+SBT_FLAGS = -ivy $(BASE_DIR)/.ivy2
+VERILATOR ?= verilator
+VERILOG_TOP = $(GEN_DIR)/PykeTop.v
 
-compile: $(gen_dir)/Pyke.v
+TOP_MODULE = PykeTop
 
-$(gen_dir)/Pyke.v: $(wildcard $(src_dir)/scala/*.scala)
-	$(SBT) $(SBT_FLAGS) "run $(gen_dir)"
+compile: $(VERILOG_TOP)
 
+$(VERILOG_TOP): $(wildcard $(SRC_DIR)/scala/*.scala)
+	$(SBT) $(SBT_FLAGS) "run $(GEN_DIR)"
+
+# Testbench
+SIM_DIR = $(BASE_DIR)/sim
+EMULATOR = $(SIM_DIR)/top-sim
+VCD_DUMP = $(SIM_DIR)/vlc_dump.vcd
+
+TB = $(BASE_DIR)/src/main/resources/csrc/tb.cc
+
+VERILATOR_FLAGS = --cc --exe --top-module $(TOP_MODULE) \
+				  --trace --trace-max-array 8192 \
+				  +define+RANDOMIZE_GARBAGE_ASSIGN \
+				  -O3
+
+.PHONY: emulator
+emulator: $(EMULATOR)
+
+$(EMULATOR): $(VERILOG_TOP) $(TB)
+	@mkdir -p $(SIM_DIR)
+	$(VERILATOR) $(VERILATOR_FLAGS) -CFLAGS "-I$(SIM_DIR)" -Mdir $(SIM_DIR) -o $@ $^
+	$(MAKE) -C $(SIM_DIR) -f V$(TOP_MODULE).mk
+
+.PHONY: clean
 clean:
-	rm -rf $(gen_dir)/*
+	rm -rf $(GEN_DIR)/*
+	rm -rf $(SIM_DIR)/*
---
+++ src/main/resources/csrc/tb.cc
@@ -0,0 +1,191 @@
+#ifdef VM_TRACE
+#include <memory>
+#include "verilated_vcd_c.h"
+#endif
+#include "verilated.h"
+#include <iostream>
+#include <fcntl.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <getopt.h>
+
+#include "VPykeTop.h"
+#define VTop VPykeTop
+
+static uint64_t trace_count = 0;
+
+double sc_time_stamp()
+{
+  return trace_count;
+}
+
+void preload_imem(VTop *top, char* path)
+{
+    FILE *f = fopen(path, "r");
+    char *line = NULL;
+    size_t len = 0;
+    ssize_t read;
+    int i = 0;
+
+    if (f == NULL) {
+        printf("Cannot read '%s' file does not exist\n", path);
+    }
+
+    while ((read = getline(&line, &len, f)) != -1) {
+//        top->PykeTop__DOT__imem__DOT__nonmasked[i] = strtol(line, NULL, 16);
+        i++;
+    }
+    fclose(f);
+}
+
+void write_dmem_word(VTop *top, uint32_t idx, uint32_t word)
+{
+//    top->PykeTop__DOT__dmem__DOT__masked_mem_0[idx] = 0xff & word;
+//    top->PykeTop__DOT__dmem__DOT__masked_mem_1[idx] = 0xff & (word >> 8) ;
+//    top->PykeTop__DOT__dmem__DOT__masked_mem_2[idx] = 0xff & (word >> 16);
+//    top->PykeTop__DOT__dmem__DOT__masked_mem_3[idx] = 0xff & (word >> 24);
+}
+
+void preload_dmem(VTop *top, char* path)
+{
+    FILE *f = fopen(path, "r");
+    char *line = NULL;
+    size_t len = 0;
+    ssize_t read;
+    int i = 0;
+
+    if (f == NULL) {
+        printf("Cannot read '%s' file does not exist\n", path);
+    }
+
+    while ((read = getline(&line, &len, f)) != -1) {
+        write_dmem_word(top, i, strtol(line, NULL, 16));
+        i++;
+    }
+    fclose(f);
+}
+
+#ifdef VM_TRACE
+void parse_args(VTop *top, VerilatedVcdC *tfp, int argc, char **argv)
+#else
+void parse_args(VTop *top, ,int argc, char **argv)
+#endif
+{
+    int opt;
+    while((opt=getopt(argc, argv, "i:d:v:")) != -1) {
+        switch(opt) {
+        case 'i':
+            printf("\n------------------------\n");
+            printf("Running %s\n", optarg);
+            printf("------------------------\n\n");
+            preload_imem(top, optarg);
+            break;
+        case 'd':
+            preload_dmem(top, optarg);
+            break;
+        case 'v':
+#ifdef VM_TRACE
+            tfp->open(optarg);
+            VL_PRINTF("Enabling waves into %s ...\n", optarg);
+#else
+            printf("Not compiled with VCD trace\n");
+#endif
+            break;
+        }
+    }
+    for(; optind < argc; optind++){
+        //printf("extra arguments: %s\n", argv[optind]);
+    }
+}
+
+void cycle_clock(VTop *top, VerilatedVcdC *tfp)
+{
+    top->clock=1;
+    top->eval();
+    trace_count++;
+#ifdef VM_TRACE
+    tfp->dump(static_cast<vluint64_t>(trace_count));
+#endif
+    top->clock=0;
+    top->eval();
+    trace_count++;
+#ifdef VM_TRACE
+    tfp->dump(static_cast<vluint64_t>(trace_count));
+#endif
+
+}
+
+void reset(VTop *top, VerilatedVcdC *tfp)
+{
+    top->reset = 1;
+    top->eval();
+#ifdef VM_TRACE
+    tfp->dump(static_cast<vluint64_t>(trace_count));
+#endif
+    trace_count++;
+
+    // chisel regs are synchronous, so we need to
+    // toggle the clock once during reset
+    cycle_clock(top, tfp);
+
+    top->reset = 0;
+    top->eval();
+#ifdef VM_TRACE
+    tfp->dump(static_cast<vluint64_t>(trace_count));
+#endif
+    trace_count++;
+}
+
+
+int main(int argc, char **argv)
+{
+    int exit_code = 0;
+#ifdef VM_TRACE
+    FILE *vcdfile = NULL;
+#endif
+    Verilated::commandArgs(argc, argv);
+    VTop *top = new VTop;
+#ifdef VM_TRACE
+    VerilatedVcdC* tfp = NULL;
+    Verilated::traceEverOn(true);  // Verilator must compute traced signals
+    tfp = new VerilatedVcdC;
+    top->trace(tfp, 99);  // Trace 99 levels of hierarchy
+    parse_args(top, tfp, argc, argv);
+#else
+    parse_args(top, argc, argv);
+#endif
+
+    reset(top, tfp);
+
+    // run a few clock cycles
+    for(int i = 0; i < 10000; i++) {
+        cycle_clock(top, tfp);
+        if (top->io_success == 2) {
+            exit_code = 0;
+            goto early_exit;
+        } else if (top->io_success == 1) {
+            printf("Failed!\n");
+            exit_code = 1;
+            goto early_exit;
+        }
+    }
+
+    if (top->io_success == 0 || top->io_success == 1) {
+        exit_code = 1;
+    }
+
+early_exit:
+#ifdef VM_TRACE
+    if (tfp)
+        tfp->close();
+    if (vcdfile)
+        fclose(vcdfile);
+#endif
+
+    delete top;
+    return exit_code;
+}
+
