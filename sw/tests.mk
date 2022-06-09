6ac8b2aa068c36aab34e52d712173237561d2f9a
Bastian Koppelmann <kbastian@mail.uni-paderborn.de>
2022-06-09 11:21:23 +0200

    tests: Allow makros to be used
    Signed-off-by: Bastian Koppelmann <kbastian@mail.uni-paderborn.de>

--- sw/tests.mk
+++ sw/tests.mk
@@ -7,7 +7,7 @@ TESTS = $(shell cd $(TEST_DIR) && find . -iname "*.s")
 TEST_VCD = $(addprefix $(TEST_BUILD_DIR)/,$(patsubst %.S, %.vcd, $(TESTS)))
 TEST_HEX = $(addprefix $(TEST_BUILD_DIR)/,$(patsubst %.S, %.hex, $(TESTS)))
 
-.PRECIOUS: $(TESTS_HEX)
+.PRECIOUS: $(TESTS_HEX) $(TEST_BUILD_DIR)/%.s
 
 .PHONY: tests clean_tests
 tests: $(TEST_BUILD_DIR) $(TEST_VCD)
@@ -15,8 +15,15 @@ tests: $(TEST_BUILD_DIR) $(TEST_VCD)
 $(TEST_BUILD_DIR):
 	@mkdir -p $(TEST_BUILD_DIR)
 
-$(TEST_BUILD_DIR)/%.hex: $(TEST_DIR)/%.S
+$(TEST_BUILD_DIR)/%.s: $(TEST_DIR)/%.S
+	@$(CC) -E -I$(TEST_DIR) -o $@ $<
+	@sed -i s/_NL/\\n/g $@
+
+$(TEST_BUILD_DIR)/%.hex: $(TEST_BUILD_DIR)/%.s
 	$(AS) -o $@ $<
 
 $(TEST_BUILD_DIR)/%.vcd:$(TEST_BUILD_DIR)/%.hex $(EMULATOR)
 	@cd $(SIM_DIR) && $(EMULATOR) -i $< -v $(patsubst %.hex, %.vcd, $<)
+
+clean_tests:
+	rm -f $(TEST_BUILD_DIR)/*
